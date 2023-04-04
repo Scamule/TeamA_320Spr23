fac10a0b22206ec3dc20e8e4536eabffd1051530
Neha Mathew <neha@vl965-172-31-198-181.wireless.umass.edu>
2023-04-04 14:23:34 -0400

    added basic date/time selector feature

--- frontend/flutter_application_1/lib/home_page.dart
+++ frontend/flutter_application_1/lib/home_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/event_adding.dart';
 import 'package:flutter_application_1/event_editing_page.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    final events = Provider.of<EventAdding>(context).events;
+
     return Scaffold(
       body: SfCalendar(
         view: CalendarView.week,
         initialSelectedDate: DateTime.now(),
         firstDayOfWeek: 7,
         headerHeight: 55,
+        //dataSource: EventDataSource(events),
       ),
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
