319d323b621efa50462e7ea3394b1386f0aac8aa
Neha Mathew <neha@vl965-172-31-198-181.wireless.umass.edu>
2023-04-04 09:41:39 -0400

    view schedule button removed

--- frontend/flutter_application_1/lib/home_page.dart
+++ frontend/flutter_application_1/lib/home_page.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/view_schedule_page.dart';
+import 'package:flutter_application_1/event_editing_page.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (BuildContext context) {
-              return const ViewSchedulePage();
-            },
-          ));
-        },
-        child: const Text('View Schedule'),
+    return Scaffold(
+      body: SfCalendar(
+        view: CalendarView.week,
+        initialSelectedDate: DateTime.now(),
+        firstDayOfWeek: 7,
+        headerHeight: 55,
       ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => EventEditingPage()))
+          //debugPrint('Clicked on button!'); },
+          ),
     );
   }
 }
