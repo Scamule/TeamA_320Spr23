17b804dbcd627be6d3fedfa6df022bf71862f2ba
KassidyKhang <kkhang@umass.edu>
2023-04-19 15:42:26 -0400

    Added profile page!

--- frontend/flutter_application_1/lib/main.dart
+++ frontend/flutter_application_1/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter_application_1/login_page.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_application_1/event_adding.dart';
 import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/profile_page.dart';
+
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
